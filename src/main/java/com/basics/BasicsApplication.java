9d8183744188b4e6bdd4bba05b9e6d8423111e5f
yongkanghuang <32233208+yongkanghuang@users.noreply.github.com>
2018-09-14 08:44:40 +0800

    first commit

---
+++ src/main/java/com/basics/BasicsApplication.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicsApplication.class, args);
+	}
+}
