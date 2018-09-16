beafcc56ffd23882f14b5585ad0de8c3d6aefe10
yongkanghuang <32233208+yongkanghuang@users.noreply.github.com>
2018-09-16 14:16:56 +0800

    加入Swagger配置

--- src/main/java/com/basics/SwaggerConfig.java
+++ src/main/java/com/basics/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.basics;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -11,18 +12,25 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+/**
+ * swagger 构建rest api 文档
+ * http://127.0.0.1:8083/basics/swagger-ui.html
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    //是否开启
+    @Value(value = "${swagger.enabled}")
+    Boolean swaggerEnabled;
+
     @Bean
-    public Docket userApi() {
+    public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("用户管理")
                 .apiInfo(apiInfo())
+                .enable(swaggerEnabled)
                 .select()
-                //.enable(false)
                 .apis(RequestHandlerSelectors.basePackage("com.test.database.controller"))
                 .paths(PathSelectors.any()).build();
     }
@@ -31,6 +39,6 @@ public class SwaggerConfig {
                 .title("Spring 中使用Swagger2构建文档")
                 .termsOfServiceUrl("https://github.com/yongkanghuang/basics")
                 .contact(new Contact("GitHub ", "https://github.com/yongkanghuang/basics", "770860791@qq.com"))
-                .version("1.1").build();
+                .version("1.0").build();
     }
 }
